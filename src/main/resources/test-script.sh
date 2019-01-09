f96795cf81c04bdc1b1d90b6f5e74b89e88d77bf
Nmikh <nmikhaluk@gmail.com>
2019-01-09 13:07:56 +0200

    script fix

--- src/main/resources/test-script.sh
+++ src/main/resources/test-script.sh
@@ -1,3 +1,6 @@
+##chmod +x /var/lib/jenkins/workspace/TestApp/src/main/resources/test-script.sh
+#BUILD_ID=dontKillMe /var/lib/jenkins/workspace/TestApp/src/main/resources/test-script.sh
+
 ##command-jenkins:: BUILD_ID=dontKillMe /var/lib/jenkins/workspace/HelloProject/src/main/resources/test-script.sh
 
 echo start
@@ -30,6 +33,8 @@ echo ” “
 #Run
 
 nohup nice java -jar $destFile –server.port=9001 &
+
+sleep 40
 
 echo “COMMAND: nohup nice java -jar $destFile “
 
